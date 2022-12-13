dbf0d1b562e768505eddc15661fa7a4d4c84ae77
Eliana Hippler <116133198+elianahippler@users.noreply.github.com>
2022-12-13 17:39:53 -0300

    .

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log(`O carro está à ${vel}Km/h`)
+
+if(vel > 60){ //condição simples
+console.log('MULTA: Você ultrapassou a velocidade máxima permitida!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'frança'
+
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil' || pais == 'brasil' || pais == 'Brazil' || pais == 'brazil'){
+    console.log('Você é brasileiro(a)!')
+}
+else {
+    console.log('Você é estrangeiro!')
+}
